320c6d18438129795f87d31d698fd98e980b3d6a
J. Regner <regnerjonas@protonmail.com>
2026-04-03 12:51:27 +0200

    feat(database): autodetect char size by default value (#2099)

--- tests/QueryStatements/CharStatementTest.php
+++ tests/QueryStatements/CharStatementTest.php
@@ -5,6 +5,7 @@ namespace Tempest\Database\Tests\QueryStatements;
 use PHPUnit\Framework\Attributes\Test;
 use PHPUnit\Framework\TestCase;
 use Tempest\Database\Config\DatabaseDialect;
+use Tempest\Database\Exceptions\DefaultValueWasInvalid;
 use Tempest\Database\QueryStatements\CharStatement;
 
 final class CharStatementTest extends TestCase
@@ -24,5 +25,55 @@ final class CharStatementTest extends TestCase
 
         $this->assertSame($expectedMysql, $statement->compile(DatabaseDialect::MYSQL));
         $this->assertSame($expectedPgsql, $statement->compile(DatabaseDialect::POSTGRESQL));
+    }
+
+    #[Test]
+    public function test_determine_char_size(): void
+    {
+        $fixedSizeStatement = new CharStatement(
+            name: 'foo',
+            size: 10,
+        );
+        $expectedMysql = '`foo` CHAR(10) NOT NULL';
+        $this->assertSame($expectedMysql, $fixedSizeStatement->compile(DatabaseDialect::MYSQL));
+
+        $defaultSizeStatement = new CharStatement(
+            name: 'foo',
+            default: 'foo_bar',
+        );
+        $expectedMysql = '`foo` CHAR(7) DEFAULT \'foo_bar\' NOT NULL';
+        $this->assertSame($expectedMysql, $defaultSizeStatement->compile(DatabaseDialect::MYSQL));
+
+        $fixedAndDefaultSizeStatement = new CharStatement(
+            name: 'foo',
+            size: 7,
+            default: 'foo_bar',
+        );
+        $expectedMysql = '`foo` CHAR(7) DEFAULT \'foo_bar\' NOT NULL';
+        $this->assertSame($expectedMysql, $fixedAndDefaultSizeStatement->compile(DatabaseDialect::MYSQL));
+    }
+
+    #[Test]
+    public function test_char_size_less_than_default_value_length(): void
+    {
+        $this->expectException(DefaultValueWasInvalid::class);
+
+        $statement = new CharStatement(
+            name: 'foo',
+            size: 1,
+            default: 'foo_bar',
+        );
+        $statement->compile(DatabaseDialect::MYSQL);
+    }
+
+    public function test_char_size_greater_than_default_value_length(): void
+    {
+        $statement = new CharStatement(
+            name: 'foo',
+            size: 10,
+            default: 'foo_bar',
+        );
+        $expectedMysql = '`foo` CHAR(10) DEFAULT \'foo_bar\' NOT NULL';
+        $this->assertSame($expectedMysql, $statement->compile(DatabaseDialect::MYSQL));
     }
 }
